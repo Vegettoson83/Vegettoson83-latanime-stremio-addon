91e35f67a7a8013b5882692053c77969bd3ace36
Vegettoson83 <154482378+Vegettoson83@users.noreply.github.com>
2025-09-09 17:39:28 -0400

    Update server.js

--- latanime-addon/server.js
+++ latanime-addon/server.js
@@ -3,6 +3,20 @@ const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 
 const PORT = process.env.PORT || 7000;
-serveHTTP(addonInterface, { port: PORT });
 
-console.log(`Latanime Stremio addon running on port ${PORT}`);
+// Wrap the AddonInterface to inject CORS headers
+function corsWrapper(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, Origin, X-Requested-With");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    return res.end();
+  }
+
+  return addonInterface(req, res); // call original addon interface
+}
+
+serveHTTP(corsWrapper, { port: PORT });
+console.log(`Latanime Stremio addon running on port ${PORT} with CORS enabled`);
