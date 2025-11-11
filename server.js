700b51069a80c80918c732f1a738f6d534cbfdde
Vegettoson83 <154482378+Vegettoson83@users.noreply.github.com>
2025-11-10 23:21:02 -0500

    Refactor server to use environment port variable

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const { serveHTTP } = require('stremio-addon-sdk');
 const addonInterface = require('./addon');
 
-serveHTTP(addonInterface, { port: 7000 });
+const PORT = process.env.PORT || 7000;
+
+serveHTTP(addonInterface, { port: PORT })
+  .then(() => console.log(`Latanime add-on running on port ${PORT}`))
+  .catch(err => console.error('Failed to start server:', err));
