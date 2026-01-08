9abfc4733d60b382caa7c09db169efdb1eac3e66
Vegettoson83 <154482378+Vegettoson83@users.noreply.github.com>
2026-01-07 19:50:18 -0500

    Change server port from 7000 to 10000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 10000;
 
 serveHTTP(addonInterface, { port: port });
 
